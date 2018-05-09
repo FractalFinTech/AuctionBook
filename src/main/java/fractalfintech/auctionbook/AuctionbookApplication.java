11347da35cba249f1de689225e5915f7f621252d
MagnusTiberius <bgonzales20001@gmail.com>
2018-05-09 09:32:29 -0500

    Init

---
+++ src/main/java/fractalfintech/auctionbook/AuctionbookApplication.java
@@ -0,0 +1,12 @@
+package fractalfintech.auctionbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionbookApplication.class, args);
+	}
+}
