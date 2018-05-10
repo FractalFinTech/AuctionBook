1b44ea0219c0cbf21c4296f093ff350533db76d9
MagnusTiberius <bgonzales20001@gmail.com>
2018-05-09 19:01:38 -0500

    Init H2

---
+++ src/main/resources/schema-h2.sql
@@ -0,0 +1,9 @@
+create sequence VENUE_SQ;
+
+  create table student
+  (
+     id integer not null,
+     name varchar(255) not null,
+     passport_number varchar(255) not null,
+     primary key(id)
+  );
